9dcf25804d7f0760e5ef968f1b12768793a1d243
Md Mahbub Alom <mdmahbuba034@gmail.com>
2024-05-10 00:32:24 +0600

    update menu

--- index.js
+++ index.js
@@ -134,6 +134,37 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/menu/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    app.get("/menu/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.patch("/menu/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          name: data.name,
+          recipe: data.recipe,
+          image: data.image,
+          category: data.category,
+          price: data.price,
+        },
+      };
+      const result = await menuCollection.updateOne(query, updatedDoc);
+      res.send(result);
+    });
+
     //reviews related api
     app.get("/reviews", async (req, res) => {
       const result = await reviewsCollection
